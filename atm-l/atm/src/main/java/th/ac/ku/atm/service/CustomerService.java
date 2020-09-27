812dc48393e86cefc3757cd729de12c59ac8cf20
Wipawee Wanwattana <wipawee.wanw@ku.th>
2020-09-27 18:00:15 +0700

    change db connection from jdbc to jpa

--- atm-l/atm/src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ atm-l/atm/src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -1,7 +1,9 @@
 package th.ac.ku.atm.service;
 
 import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import th.ac.ku.atm.data.CustomerRepository;
 import th.ac.ku.atm.model.Customer;
 
 import javax.annotation.PostConstruct;
@@ -12,31 +14,31 @@ import java.util.List;
 @Service
 public class CustomerService {
 
-    private List<Customer> customerList ;
 
-    @PostConstruct
-    public void postConstruct(){
-        this.customerList = new ArrayList<>();
+    private CustomerRepository repository;
 
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
     }
 
     public  void createCustomer(Customer customer){
         //hashpin for customer
         String hashPin = hash(customer.getPin());
         customer.setPin(hashPin);
-        customerList.add(customer);
+        repository.save(customer);
     }
 
     public List<Customer> getCustomerList(){
-        return  new ArrayList<>(this.customerList);
+
+        return repository.findAll();
     }
 
-    public Customer findCustomer(int id){
-        for (Customer customer: customerList){
-            if(customer.getId() == id)
-                return customer;
+    public Customer findCustomer(int id) {
+        try {
+            return repository.findById(id);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
         }
-        return null;
     }
 
     public Customer checkPin(Customer inputCustomer){
